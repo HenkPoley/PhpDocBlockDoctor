56e375582fd64a4aa52d589bf6aeee9de171e576
Henk Poley <HenkPoley@gmail.com>
2025-06-04 07:12:00 +0200

    Check in failing test fixture.
    On $tryLanguages = $this->translator->getLanguage()->getPreferredLanguages(); we seem to track until ->getLanguage(), but we currently do not understand where ->getPreferredLanguages() lives, so we don't know that its exceptions need to be added to the `@throws`.

---
+++ tests/fixtures/function-call-on-object-returned-from-function/Template.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Pitfalls\FunctionCallOnObjectReturnedFromFunction;
+
+/** Found in \SimpleSAML\XHTML\Template::getEntityPropertyTranslation() */
+class Template
+{
+    /**
+     * @var Translate
+     */
+    private Translate $translator;
+
+    public function __construct()
+    {
+        $this->translator = new Translate();
+    }
+
+    /**
+     * Make sure we actually understand where the function ->getPreferredLanguages() lives
+     */
+    public function getEntityPropertyTranslation(): void
+    {
+        $tryLanguages = $this->translator->getLanguage()->getPreferredLanguages();
+    }
+}
+
+class Translate
+{
+    private Language $language;
+
+    public function __construct()
+    {
+        $this->language = new Language();
+    }
+
+    public function getLanguage(): Language
+    {
+        return $this->language;
+    }
+}
+
+class Language
+{
+    /**
+     * Just some Error gets thrown, could be any Throwable.
+     *
+     * @return array
+     * @throws \ArithmeticError
+     */
+    public function getPreferredLanguages(): array
+    {
+        throw new \ArithmeticError();
+    }
+}
