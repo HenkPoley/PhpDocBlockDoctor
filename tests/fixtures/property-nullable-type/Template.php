795b6f25d0d85e94f26c3f74349e6f912de05f56
Henk Poley <HenkPoley@gmail.com>
2025-06-07 15:08:42 +0200

    Add coverage for nullable typed property

---
+++ tests/fixtures/property-nullable-type/Template.php
@@ -0,0 +1,44 @@
+<?php
+namespace Pitfalls\PropertyNullableType;
+
+class Template
+{
+    private ?Translate $translator;
+
+    public function __construct()
+    {
+        $this->translator = new Translate();
+    }
+
+    public function getEntityPropertyTranslation(): void
+    {
+        $this->translator->getLanguage()->getPreferredLanguages();
+    }
+}
+
+class Translate
+{
+    private Language $language;
+
+    public function __construct()
+    {
+        $this->language = new Language();
+    }
+
+    public function getLanguage(): Language
+    {
+        return $this->language;
+    }
+}
+
+class Language
+{
+    /**
+     * @throws \ArithmeticError
+     * @return array
+     */
+    public function getPreferredLanguages(): array
+    {
+        throw new \ArithmeticError();
+    }
+}
